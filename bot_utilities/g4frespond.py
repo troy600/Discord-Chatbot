671d0706e4d15ae46d54fab592354650a7811c52
troy600 <bazartroy531@gmail.com>
2024-09-12 18:10:19 +0800

    changeson g4f responds

--- bot_utilities/g4frespond.py
+++ bot_utilities/g4frespond.py
@@ -2,23 +2,21 @@ from g4f.client import AsyncClient
 from g4f.Provider import HuggingChat, HuggingFace
 import asyncio
 from bot_utilities.config_loader import load_current_language, config
-from bot_utilities.ai_utils import search
-
 client = AsyncClient(
     provider=HuggingChat
 )
 
-
 async def huggingchat(persona, history, search):
+    search_result = search if search is not None else "search results is disabled"
     messages = [
         {"role": "system", "name": "instructions", "content": persona},
         *history,
-        {"role": "assistant", "search": "result", "content": search}
+        {"role": "assistant", "search": "result", "content": search_result}
 
     ]
     response = await client.chat.completions.create(
-        #model=config['GPT_MODEL'],
-        model="mixtral-x87b",
+        model=config['GPT_MODEL'],
+        #model="mixtral-x87b",
         messages=messages
     )
     message = response.choices[0].message.content
