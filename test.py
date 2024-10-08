3662af852a11b1c7e2a7ec1dcd62c30ac0f3f1c5
troy600 <bazartroy531@gmail.com>
2024-10-08 19:00:35 +0800

    test.py

--- test.py
+++ test.py
@@ -1,37 +1,11 @@
-from openai import AsyncOpenAI
-from g4f.client import AsyncClient
-from g4f.Provider import HuggingChat
-import os
-from dotenv import load_dotenv # python-dotenv
-import asyncio
+from g4f.client import Client
 
-print(load_dotenv())
-
-client = AsyncOpenAI(api_key=os.getenv("CHIMERA_GPT_KEY"), base_url="https://api.naga.ac/v1/")
-g4fclient = AsyncClient(provider=HuggingChat)
-
-async def g4fmsg(message):
-    msg = [{"role": "user", "content": message}]
-    response = await g4fclient.chat.completions.create(
-        model="mistral-7b",
-        messages=msg
-    )
-    return response.choices[0].message.content
-
-async def chatcompletion(mesg):
-    msg = [{"role": "user", "content": mesg}]
-    response = await client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=msg,
-    )
-
-    return response.choices[0].message.content
-
-lsa = input("\n >>")
-print(asyncio.run(g4fmsg(message=lsa)))
-
-'''
-def main():
-    lsa = input("\n >>")
-    print(asyncio.run(chatcompletion(mesg=lsa)))
-'''
+client = Client()
+response = client.images.generate(
+  model="flux",
+  prompt="a very cute anime girl",
+  width=777,
+  height=1024
+)
+image_url = response.data[0].url
+print(image_url)
