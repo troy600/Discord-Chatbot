b3cc1a24a15159a342b74e0baf11cdc666bddb7a
troy600 <bazartroy531@gmail.com>
2024-10-18 13:41:52 +0800

    test.pyyyyyy

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
-from g4f.client import Client
+import asyncio
 
-client = Client()
-response = client.images.generate(
-  model="flux",
-  prompt="a very cute anime girl",
-  width=777,
-  height=1024
-)
-image_url = response.data[0].url
-print(image_url)
+this = ""
+
+from g4f.client import AsyncClient
+
+async def main():
+    userin = input(">>")
+    client = AsyncClient()
+    stream = await client.chat.completions.create(
+        model="mistral-7b",
+        messages=[{"role": "user", "content": f"{userin}"}],
+        stream=True,
+        # Add any other necessary parameters
+    )
+    async for chunk in stream:
+        if chunk.choices[0].delta.content:
+            print(chunk.choices[0].delta.content or "", end="")
+
+asyncio.run(main())
