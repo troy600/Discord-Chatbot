5b00d1cccd7b4126f8955bf6eace351210620161
troy600 <bazartroy531@gmail.com>
2024-10-21 17:43:06 +0800

    Youtube dl updates

--- bot_utilities/youtubedl.py
+++ bot_utilities/youtubedl.py
@@ -1,7 +1,12 @@
 import os
 import subprocess
 import asyncio
+import random
 
+tempmusic = random.randint(1, 2999)
+tempthumb = random.randint(1, 3999)
+tempmusic2 = random.randint(1, 2999)
+tempmusic3 = random.randint(1, 3999)
 
 async def bash_command(cmd):
     process = await asyncio.create_subprocess_shell(
@@ -21,32 +26,21 @@ async def bash_command(cmd):
 delfile = '/tmp/21.png'
 delfile2 = "/tmp/imafile.webp"
 
-def removes():
-    for keycontext in os.listdir("/tmp"):
-        if keycontext == '21.png':
-            os.remove(f"{delfile}")
-        elif keycontext == 'imafile.webp':
-            os.remove(delfile2)
-        elif keycontext == 'pske.m4a':
-            os.remove("/tmp/pske.m4a")
-        elif keycontext == "pske.mp3":
-            os.remove("/tmp/pske.mp3")
-
 
 #magic wahahahah
 async def download_thumbnail(flink):
-    await bash_command(f"/usr/bin/yt-dlp '{flink}' --embed-thumbnail --no-download  -o /tmp/imafile &")
+    await bash_command(f"/usr/bin/yt-dlp '{flink}' --embed-thumbnail --no-download  -o ./temp/{tempthumb}")
 
 async def download_m4a(flink):
-    await bash_command(f'yt-dlp -f140 "{flink}" --embed-metadata --parse-metadata "playlist_index:%(track_number)s" -o /tmp/pske.m4a')
+    await bash_command(f'yt-dlp -f140 "{flink}" --embed-metadata --parse-metadata "playlist_index:%(track_number)s" -o ./temp/{tempmusic}.m4a')
 
 async def ffmpeg_function(namemusic):
-    await bash_command(f'ffmpeg -i /tmp/imafile.webp -vf "crop=w=min(min(iw\,ih)\,720):h=min(min(iw\,ih)\,720),scale=720:720,setsar=1" -vframes 1 /tmp/21.png')
-    await bash_command("ffmpeg -i /tmp/pske.m4a -c:a libmp3lame /tmp/pske.mp3")
-    await bash_command(f"ffmpeg -i /tmp/pske.mp3 -i /tmp/21.png -map 1:0 -map 0:0 -c copy {namemusic}")
+    await bash_command(f'ffmpeg -i ./temp/{tempthumb}.webp -vf "crop=w=min(min(iw\,ih)\,720):h=min(min(iw\,ih)\,720),scale=720:720,setsar=1" -vframes 1 ./temp/{tempmusic2}.png')
+    await bash_command(f"ffmpeg -i ./temp/{tempmusic}.m4a -c:a libmp3lame ./temp/{tempmusic3}.mp3")
+    await bash_command(f"ffmpeg -i ./temp/{tempmusic3}.mp3 -i ./temp/{tempmusic2}.png -map 1:0 -map 0:0 -c copy './temp/{namemusic}.mp3'")
 
 async def func(links):
-    removes()
+    await bash_command("rm -rvf ./temp/*")
     await asyncio.gather(download_thumbnail(links), download_m4a(links))
 
 async def thefunc(link, music_name):
