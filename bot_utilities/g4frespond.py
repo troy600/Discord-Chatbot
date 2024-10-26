be5664e588e888c4841bd074c3f832f5a653873b
troy600 <bazartroy531@gmail.com>
2024-10-26 20:22:07 +0800

    g4f response changes

--- bot_utilities/g4frespond.py
+++ bot_utilities/g4frespond.py
@@ -2,10 +2,38 @@ from g4f.client import AsyncClient
 from g4f.Provider import HuggingChat, HuggingFace
 import asyncio
 from bot_utilities.config_loader import load_current_language, config
+import os
+
+thehftokenusedonyouraccount = os.getenv("HF")
+
 client = AsyncClient(
     provider=HuggingChat
 )
+'''
+from huggingface_hub import AsyncInferenceClient
 
+async def huggingchat(persona, history, search):
+    print(history)
+    model_id="mistralai/Mistral-Nemo-Instruct-2407"
+    client = AsyncInferenceClient(api_key=thehftokenusedonyouraccount)
+
+    search_result = f"Search_Results: {search}" if search is not None else ""
+
+    messages = [
+        { "role": "system", "content": persona },
+	    *history,
+#        {"role": "assistant", "search": "result", "content": search_result}
+    ]
+    output = await client.chat.completions.create(
+        model=model_id, 
+        messages=messages,
+        temperature=0.5,
+        top_p=0.7
+    )
+
+    return output.choices[0].message.content
+
+'''
 async def huggingchat(persona, history, search):
     search_result = search if search is not None else "search results is disabled"
     messages = [
@@ -21,5 +49,6 @@ async def huggingchat(persona, history, search):
     )
     message = response.choices[0].message.content
     return message
+
 
 print("using g4f!!")
