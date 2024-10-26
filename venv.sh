8e123b90da3b317b61fe75803e6749de55298abb
troy600 <bazartroy531@gmail.com>
2024-10-26 20:25:46 +0800

    venvvvvvvvvvvvv

--- venv.sh
+++ venv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-exec /usr/bin/python -m venv venv
+exec /usr/bin/python -m venv ./venv
 echo "source ./venv/bin/activate" >> activate.sh
 echo "use bash activate.sh to activate the venv"
